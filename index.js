e4abb78d6945a30ee36065b6c9c1e0d897bb1c23
Diogo Deconto <dideconto@gmail.com>
2020-08-13 20:29:10 -0300

    Criação do servidor

---
+++ index.js
@@ -0,0 +1,15 @@
+"use strict";
+
+//Declarar as dependências do arquivo
+const express = require("express");
+const app = express();
+
+
+
+//Definir que o servidor vai escutar
+const port = 1234;
+
+//Configurando o servidor para escutar a porta definida
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
